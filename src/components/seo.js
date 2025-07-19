24bf5671cee23f9a8b66407bc312b7ec29c8241a
Kevin <kev.gri32@gmail.com>
2025-07-18 19:31:38 -0700

    Updated site meta data for seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,13 +1,11 @@
 /**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
+ * SEO component that queries for data with Gatsby's useStaticQuery React hook
  *
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
 import React from "react"
 import PropTypes from "prop-types"
-import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
 function Seo({ description, lang, meta, keywords, title }) {
@@ -28,56 +26,19 @@ function Seo({ description, lang, meta, keywords, title }) {
   const metaDescription = description || site.siteMetadata.description
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata.author,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ]
-        .concat(
-          keywords.length > 0
-            ? {
-                name: `keywords`,
-                content: keywords.join(`, `),
-              }
-            : []
-        )
-        .concat(meta)}
-    />
+    <>
+      <html lang={lang} />
+      <title>{`${title} | ${site.siteMetadata.title}`}</title>
+      <meta name={"og:title"} content={title} />
+      <meta name={"description"} content={metaDescription} />
+      <meta name={"og:description"} content={metaDescription} />
+      <meta name={"og:type"} content={"website"} />
+      <meta name={"twitter:card"} content={"summary"} />
+      <meta name={"twitter:creator"} content={site.siteMetadata.author} />
+      <meta name={"twitter:title"} content={title} />
+      <meta name={"twitter:description"} content={metaDescription} />
+      <meta name={"keywords"} content={keywords.join(`, `)} />
+    </>
   )
 }
 
